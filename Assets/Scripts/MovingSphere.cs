7d09476959eac63be96f82f59d6132463e65510e
Conor Byrne <cjjb95@gmail.com>
2020-10-23 13:48:53 +0100

    Beginning work on physics based movement.
    -Added RigidBody for sphere.
    -Removed allowed area from MovingSphere.cs
    -Removed bounciness

--- Assets/Scripts/MovingSphere.cs
+++ Assets/Scripts/MovingSphere.cs
@@ -9,11 +9,7 @@ public class MovingSphere : MonoBehaviour
 	[SerializeField, Range(0f, 100f)]
 	float maxAcceleration = 10f;
 	Vector3 velocity;
-	[SerializeField, Range(0f, 1f)]
-	float bounciness = 0.5f; //bounciness is used to radically reduce the velocity of the ball
 
-	[SerializeField]
-	Rect allowedArea = new Rect(-5f, -5f, 10f, 10f); //creates rectangle with an origin (x & y position), a width & a height
 	void Update()
 	{
 		Vector2 playerInput;
@@ -28,29 +24,8 @@ public class MovingSphere : MonoBehaviour
 
 		Vector3 displacement = velocity * Time.deltaTime; //Displacement takes the velocity and multiplies it by Delta Time, this is how much the ball will move in the next frame
 
-
 		Vector3 newPosition = transform.localPosition + displacement; // New position is gotten from the balls current position + the displacement vector
 
-		if (newPosition.x < allowedArea.xMin) //if the ball comes in contact with the edges of the allowed area then its velocity is now flipped and multiplied by bounciness
-		{
-			newPosition.x = allowedArea.xMin;
-			velocity.x = -velocity.x * bounciness;
-		}
-		else if (newPosition.x > allowedArea.xMax)
-		{
-			newPosition.x = allowedArea.xMax;
-			velocity.x = -velocity.x * bounciness;
-		}
-		if (newPosition.z < allowedArea.yMin)
-		{
-			newPosition.z = allowedArea.yMin;
-			velocity.z = -velocity.z * bounciness;
-		}
-		else if (newPosition.z > allowedArea.yMax)
-		{
-			newPosition.z = allowedArea.yMax;
-			velocity.z = -velocity.z * bounciness;
-		}
 		transform.localPosition = newPosition;
 	}
 }
