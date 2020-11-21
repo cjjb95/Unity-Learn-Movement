051a0e8ff34e3c03d184b72ecc388c33ca144621
Conor Byrne <cjjb95@gmail.com>
2020-11-21 14:58:43 +0000

    Jumps are now physics based
    -The spheres jump height is now configurable in the game editor, it has a max height of 5.
    -The sphere will jump using the gravitational formula sqrt of -2gh, this will add and redcuce speed over the course of a jump.
    -Before the sphere jumps for hte first time it now checks if it's touching the ground first, it cannot jump off of walls as it is checking the normal vectors of where it is touching
    -The sphere is now capable of a limited amount of jumps in the air, this is handled in the Jump method.
    -The sphere now has a limit on how fast it can go up in the air, this is defined witht he maxAirAcceleration variable

--- Assets/Scripts/MovingSphere.cs
+++ Assets/Scripts/MovingSphere.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class MovingSphere : MonoBehaviour
 {
+	[SerializeField, Range(0f, 10f)]
+	float jumpHeight = 2f;
+
+	[SerializeField, Range(0, 5)]
+	int maxAirJumps = 0;
+
 	[SerializeField, Range(0f, 100f)]
 	float maxSpeed = 10f;
 	[SerializeField, Range(0f, 100f)]
-	float maxAcceleration = 10f;
+	float maxAcceleration = 10f, maxAirAcceleration = 1f;
 	Vector3 velocity, desiredVelocity;
 	Rigidbody body;
 	bool desiredJump;
+	bool onGround;
+	int jumpPhase;
+
 
 	void Awake()
 	{
@@ -30,8 +39,9 @@ public class MovingSphere : MonoBehaviour
 
 	void FixedUpdate()
 	{
-		velocity = body.velocity;
-		float maxSpeedChange = maxAcceleration * Time.deltaTime;
+		UpdateState();
+		float acceleration = onGround ? maxAcceleration : maxAirAcceleration;
+		float maxSpeedChange = acceleration * Time.deltaTime;
 		velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
 		velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
 
@@ -40,13 +50,50 @@ public class MovingSphere : MonoBehaviour
 			desiredJump = false;
 			Jump();
 		}
-
 		body.velocity = velocity;
+		onGround = false;
+	}
+
+	void UpdateState()
+	{
+		velocity = body.velocity;
+		if (onGround)
+		{
+			jumpPhase = 0;
+		}
 	}
 
 	void Jump()
 	{
-		velocity.y += 5f;
+		if (onGround || jumpPhase < maxAirJumps)
+		{
+			jumpPhase += 1;
+			float jumpSpeed = Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
+			if (velocity.y > 0f)
+			{
+				jumpSpeed = Mathf.Max(jumpSpeed - velocity.y, 0f);
+			}
+			velocity.y += jumpSpeed;
+		}
+
 	}
 
+	void OnCollisionEnter(Collision collision)
+	{
+		EvaluateCollision(collision);
+	}
+
+	void OnCollisionStay(Collision collision)
+	{
+		EvaluateCollision(collision);
+	}
+
+	void EvaluateCollision(Collision collision) {
+		for (int i = 0; i < collision.contactCount; i++)
+		{
+			Vector3 normal = collision.GetContact(i).normal;
+			onGround |= normal.y >= 0.9f;
+		}
+	
+	}
 }
